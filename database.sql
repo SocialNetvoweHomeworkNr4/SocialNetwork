13e7b2d396edd369f82626f158d0ae21687f60e5
Arvils Mačāns <arva10@inbox.lv>
2016-01-15 20:10:25 +0200

    Fix database import query

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `linkme` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `linkme`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win32 (AMD64)
 --
 -- Host: 127.0.0.1    Database: linkme
@@ -55,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-01-15 19:10:28
+-- Dump completed on 2016-01-15 20:07:26
