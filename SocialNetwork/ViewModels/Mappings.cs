d4f29f76788cd056fb28d1cbbca660900662a3fd
Artem Savostianov <qno@seanv.ru>
2016-01-17 01:04:55 +0200

    Nugets added to ignore list

---
+++ SocialNetwork/ViewModels/Mappings.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.ViewModels
+{
+    public static class AutoMapperWebConfiguration
+    {
+        public static void Configure()
+        {
+            ConfigureUserMapping();
+        }
+
+        private static void ConfigureUserMapping()
+        {
+
+            Mapper.CreateMap<UserImage, UserImageViewModel>();
+            Mapper.CreateMap<UserImageViewModel, UserImage>();
+        }
+    }
+}
