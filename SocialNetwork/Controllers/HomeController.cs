d5be28683d452f4dbbadd153128d5c159ecefbeb
Arvils M <arva10@inbox.lv>
2016-01-18 00:15:58 +0200

    Removing obselete pages and content from home page.

--- SocialNetwork/Controllers/HomeController.cs
+++ SocialNetwork/Controllers/HomeController.cs
@@ -16,21 +16,5 @@ namespace SocialNetwork.Controllers
         {
             return View();
         }
-
-        [Authorize]
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
